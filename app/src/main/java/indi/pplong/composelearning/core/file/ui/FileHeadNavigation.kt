20465a6bfdbab70db2148525e0c7c4e99b4640ff
PPLong <pplong222@outlook.com>
2024-10-30 21:05:57 -0400

    [Dependency]: Update compose-material3 dependency.

--- app/src/main/java/indi/pplong/composelearning/core/file/ui/FileHeadNavigation.kt
+++ app/src/main/java/indi/pplong/composelearning/core/file/ui/FileHeadNavigation.kt
@@ -11,12 +11,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -92,7 +92,7 @@ fun SinglePathText(
     pathText: String, onButtonClick: () -> Unit = {}
 ) {
     val source = remember { MutableInteractionSource() }
-    val ripple = rememberRipple(color = Color.Gray, bounded = true) // 设置涟漪效果的颜色
+    val ripple = ripple(color = Color.Gray, bounded = true) // 设置涟漪效果的颜色
 
     Card(
         shape = RoundedCornerShape(size = 8.dp),
